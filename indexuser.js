d7ff354107fe308a2fdafb7d61aa29d17b3d5a52
davidriverag <87343870+davidriverag@users.noreply.github.com>
2021-08-31 17:19:34 -0600

    Add files via upload

---
+++ indexuser.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const Sequelize = require('sequelize')
+const app = express()
+const port = 3000
+app.use(express.json());
+//const sequelize = new Sequelize('postgres://david:open97@192.168.0.19:5432/ejemplodb')
+const sequelize = new Sequelize('ejemplodb','david','david97', {host: '192.168.0.19', dialect: 'postgres'})
+sequelize
+.authenticate()
+.then(() => {
+console.log('Connection has been established successfully.');
+})
+.catch(err => {
+console.error('Unable to connect to the database:', err);
+});
+const User = sequelize.define('user', {
+// attributes
+firstName: {
+type: Sequelize.STRING,
+allowNull: false
+},
+lastName: {
+type: Sequelize.STRING
+// allowNull defaults to true
+}
+}, {
+// options
+});
+// Note: using `force: true` will drop the table if it already exists
+User.sync({ force: true }) // Now the `users` table in the database corresponds to the model definition
+app.get('/', (req, res) => res.json({ message: 'Hello World' }))
+app.post('/user', async (req, res) => {
+try {
+const newUser = new User(req.body)
+await newUser.save()
+res.json({ user: newUser }) // Returns the new user that is created in the database
+} catch(error) {
+console.error(error)
+}
+})
+app.get('/user/:userId', async (req, res) => {
+const userId = req.params.userId
+try {
+const user = await User.findAll({
+where: {
+id: userId
+}
+}
+)
+res.json({ user })
+} catch(error) {
+console.error(error)
+}
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+ 
